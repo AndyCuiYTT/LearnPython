e3b0a9440cedb6554af7c2ed4f9e2a27fce6b5fe
AndyCui <AndyCuiYTT@163.com>
2017-10-14 14:36:07 +0800

    django 初次尝试

--- Django_1/bookTest/views.py
+++ Django_1/bookTest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BookInfo, HeroInfo
 
 
 # Create your views here.
@@ -9,4 +10,18 @@ from django.http import HttpResponse
 # HttpRequest
 def index(request):
     # HttpResponse
-    return HttpResponse('hello Django')
+    # return HttpResponse('hello Django')
+    context = {'title': '首页', 'list': range(10)}
+    return render(request, 'bookTest/index.html', context)
+
+
+def bookList(request):
+    list = BookInfo.objects.all()
+    context = {'bookList': list}
+    return render(request, 'bookTest/bookList.html', context)
+
+
+def detail(request, id):
+    list = BookInfo.objects.get(id=id).heroinfo_set.all()
+    context = {'herolist': list}
+    return render(request, 'bookTest/detail.html', context)
